9b9fbdb3c44254bf6018692ce52fec0401707fb9
Albert Visser <albert@lemoncurry>
2014-08-16 22:22:41 +0200

    latest fixes

--- magiokis/views.py
+++ magiokis/views.py
@@ -3,7 +3,7 @@
 ## from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
-from settings import MEDIA_ROOT
+from magiokis.settings import MEDIA_ROOT
 
 def index(request):
     return render_to_response('index.html',{})
