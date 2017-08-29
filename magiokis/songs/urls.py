06ba87002f5ab5a5958b717686da10e99a6f381e
Albert Visser <albert@lemoncurry>
2017-08-29 20:21:06 +0200

    replaced strings with url instances

--- magiokis/songs/urls.py
+++ magiokis/songs/urls.py
@@ -4,64 +4,64 @@ from django.conf.urls import *
 
 urlpatterns = patterns(
     'magiokis.songs.views',
-    (r'^$', 'index'),
-    (r'^select/$', 'songlist'),
-    (r'^select/(?P<soort>(letter|jaar|search))/$', 'songlist'),
-    (r'^select/(?P<soort>(letter|jaar|search))/(?P<data>(\w|\s)+)/$', 'songlist'),
-    (r'^select/opnames/$', 'opnlist'),
-    (r'^select/opnames/(?P<item>\d+)/$', 'opnlist'),
-    (r'^select/regs/$', 'reglist'),
-    (r'^select/regs/(?P<item>\d+)/$', 'reglist'),
-    (r'^select/series/$', 'series'),
-    (r'^detail/(?P<action>add)/$', 'detail'),
-    (r'^detail/(?P<item>\d+)/$', 'detail'),
-    (r'^detail/(?P<item>\d+)/(?P<soort>(jaar|letter|search))/(?P<sel>(\w|\s)+)/$',
+    url(r'^$', 'index'),
+    url(r'^select/$', 'songlist'),
+    url(r'^select/(?P<soort>(letter|jaar|search))/$', 'songlist'),
+    url(r'^select/(?P<soort>(letter|jaar|search))/(?P<data>(\w|\s)+)/$', 'songlist'),
+    url(r'^select/opnames/$', 'opnlist'),
+    url(r'^select/opnames/(?P<item>\d+)/$', 'opnlist'),
+    url(r'^select/regs/$', 'reglist'),
+    url(r'^select/regs/(?P<item>\d+)/$', 'reglist'),
+    url(r'^select/series/$', 'series'),
+    url(r'^detail/(?P<action>add)/$', 'detail'),
+    url(r'^detail/(?P<item>\d+)/$', 'detail'),
+    url(r'^detail/(?P<item>\d+)/(?P<soort>(jaar|letter|search))/(?P<sel>(\w|\s)+)/$',
      'detail'),
-    (r'^detail/(?P<item>\d+)/(?P<action>edit)/$', 'detail'),
-    (r'^detail/(?P<item>\d+)/(?P<action>edit)/(?P<soort>(jaar|letter|search))/'
+    url(r'^detail/(?P<item>\d+)/(?P<action>edit)/$', 'detail'),
+    url(r'^detail/(?P<item>\d+)/(?P<action>edit)/(?P<soort>(jaar|letter|search))/'
      '(?P<sel>(\w|\s)+)/$', 'detail'),
-    (r'^detail/(?P<item>\d+)/update/$', 'wijzigdetail'),
-    (r'^detail/(?P<item>\d+)/update/(?P<soort>(jaar|letter|search))/'
+    url(r'^detail/(?P<item>\d+)/update/$', 'wijzigdetail'),
+    url(r'^detail/(?P<item>\d+)/update/(?P<soort>(jaar|letter|search))/'
      '(?P<sel>(\w|\s)+)/$', 'wijzigdetail'),
-    (r'^tekst/(?P<item>\d+)/(?P<action>(add|edit))/$', 'wijzigtekst'),
-    (r'^tekst/(?P<item>\d+)/(?P<action>(add|edit))/(?P<soort>(jaar|letter|search))/'
+    url(r'^tekst/(?P<item>\d+)/(?P<action>(add|edit))/$', 'wijzigtekst'),
+    url(r'^tekst/(?P<item>\d+)/(?P<action>(add|edit))/(?P<soort>(jaar|letter|search))/'
      '(?P<sel>(\w|\s)+)/$', 'wijzigtekst'),
-    (r'^tekst/(?P<item>\d+)/update/$', 'wijzigtekst'),
-    (r'^tekst/(?P<item>\d+)/update/(?P<soort>(jaar|letter|search))/'
+    url(r'^tekst/(?P<item>\d+)/update/$', 'wijzigtekst'),
+    url(r'^tekst/(?P<item>\d+)/update/(?P<soort>(jaar|letter|search))/'
      '(?P<sel>(\w|\s)+)/$', 'wijzigtekst'),
-    (r'^(?P<item>\d+)/opname/(?P<action>add)/$', 'opname'),
-    (r'^(?P<item>\d+)/opname/(?P<action>add)/(?P<soort>(jaar|letter|search))/'
+    url(r'^(?P<item>\d+)/opname/(?P<action>add)/$', 'opname'),
+    url(r'^(?P<item>\d+)/opname/(?P<action>add)/(?P<soort>(jaar|letter|search))/'
      '(?P<sel>(\w|\s)+)/$', 'opname'),
-    (r'^(?P<item>\d+)/opname/(?P<action>add)/(?P<melding>(\w|\s)+)$', 'opname'),
-    # (r'^(?P<song>\d+)/opname/(?P<item>\d+)/$',   'opname'),
-    # (r'^(?P<song>\d+)/opname/(?P<item>\d+)/update/$', 'wijzigopname'),
-    # (r'^(?P<song>\d+)/opname/play/(?P<item>\w+)/$', 'playopname'),
-    (r'^opname/(?P<item>\d+)/$', 'opname'),
-    (r'^opname/(?P<item>\d+)/(?P<soort>(jaar|letter|search))/(?P<sel>(\w|\s)+)/$',
+    url(r'^(?P<item>\d+)/opname/(?P<action>add)/(?P<melding>(\w|\s)+)$', 'opname'),
+    # url(r'^(?P<song>\d+)/opname/(?P<item>\d+)/$',   'opname'),
+    # url(r'^(?P<song>\d+)/opname/(?P<item>\d+)/update/$', 'wijzigopname'),
+    # url(r'^(?P<song>\d+)/opname/play/(?P<item>\w+)/$', 'playopname'),
+    url(r'^opname/(?P<item>\d+)/$', 'opname'),
+    url(r'^opname/(?P<item>\d+)/(?P<soort>(jaar|letter|search))/(?P<sel>(\w|\s)+)/$',
      'opname'),
-    (r'^opname/(?P<item>\d+)/update/$', 'wijzigopname'),
-    (r'^opname/(?P<item>\d+)/update/(?P<soort>(jaar|letter|search))/'
+    url(r'^opname/(?P<item>\d+)/update/$', 'wijzigopname'),
+    url(r'^opname/(?P<item>\d+)/update/(?P<soort>(jaar|letter|search))/'
      '(?P<sel>(\w|\s)+)/$', 'wijzigopname'),
-    # (r'^opname/play/(?P<item>\w+)/$', 'playopname'), -- vervangen door xspf player
-    (r'^(?P<item>\d+)/reg/(?P<action>add)/$', 'reg'),
-    (r'^(?P<item>\d+)/reg/(?P<action>add)/(?P<soort>(jaar|letter|search))/'
+    # url(r'^opname/play/(?P<item>\w+)/$', 'playopname'), -- vervangen door xspf player
+    url(r'^(?P<item>\d+)/reg/(?P<action>add)/$', 'reg'),
+    url(r'^(?P<item>\d+)/reg/(?P<action>add)/(?P<soort>(jaar|letter|search))/'
      '(?P<sel>(\w|\s)+)/$', 'reg'),
     # (r'^(?P<song>\d+)/reg/(?P<item>\d+)/$',   'reg'),
     # (r'^(?P<song>\d+)/reg/(?P<item>\d+)/update/$', 'wijzigreg'),
     # (r'^(?P<song>\d+)/reg/play/(?P<item>\w+)/$', 'playreg'),
-    (r'^reg/(?P<item>\d+)/$', 'reg'),
-    (r'^reg/(?P<item>\d+)/(?P<soort>(jaar|letter|search))/(?P<sel>(\w|\s)+)/$',
+    url(r'^reg/(?P<item>\d+)/$', 'reg'),
+    url(r'^reg/(?P<item>\d+)/(?P<soort>(jaar|letter|search))/(?P<sel>(\w|\s)+)/$',
      'reg'),
-    (r'^reg/(?P<item>\d+)/update/$', 'wijzigreg'),
-    (r'^reg/(?P<item>\d+)/update/(?P<soort>(jaar|letter|search))/'
+    url(r'^reg/(?P<item>\d+)/update/$', 'wijzigreg'),
+    url(r'^reg/(?P<item>\d+)/update/(?P<soort>(jaar|letter|search))/'
      '(?P<sel>(\w|\s)+)/$', 'wijzigreg'),
-    (r'^reg/show/(?P<item>\w+)/$', 'showreg'),
-    (r'^reg/show/(?P<item>\w+)/(?P<page>\w+)/$', 'showreg'),
-    (r'^tabel/(?P<soort>\w+)/$', 'tabel'),
-    (r'^tabel/(?P<soort>\w+)/(?P<item>\d+)/update/$', 'wijzigtabel'),
-    (r'^tabel/(?P<soort>\w+)/add/$', 'wijzigtabel'),
-    # (r'^play/(?P<type>\w+)/(?P<id>\d+)/(?P<titel>\d+)/$',        'playurl'),
+    url(r'^reg/show/(?P<item>\w+)/$', 'showreg'),
+    url(r'^reg/show/(?P<item>\w+)/(?P<page>\w+)/$', 'showreg'),
+    url(r'^tabel/(?P<soort>\w+)/$', 'tabel'),
+    url(r'^tabel/(?P<soort>\w+)/(?P<item>\d+)/update/$', 'wijzigtabel'),
+    url(r'^tabel/(?P<soort>\w+)/add/$', 'wijzigtabel'),
+    # url(r'^play/(?P<type>\w+)/(?P<id>\d+)/(?P<titel>\d+)/$',        'playurl'),
 
     # Uncomment this for admin:
-    # (r'^admin/', include('django.contrib.admin.urls')),
+    # url(r'^admin/', include('django.contrib.admin.urls')),
 )
--- magiokis/urls.py
+++ magiokis/urls.py
@@ -9,34 +9,34 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     # Example:
-    # (r'^magiokis/', include('magiokis.foo.urls')),
-    (r'^$', 'magiokis.views.index'),
-    (r'^denk/', include('magiokis.denk.urls')),
-    (r'^vertel/', include('magiokis.vertel.urls')),
-    (r'^songs/', include('magiokis.songs.urls')),
+    # url(r'^magiokis/', include('magiokis.foo.urls')),
+    url(r'^$', 'magiokis.views.index'),
+    url(r'^denk/', include('magiokis.denk.urls')),
+    url(r'^vertel/', include('magiokis.vertel.urls')),
+    url(r'^songs/', include('magiokis.songs.urls')),
     # Uncomment this for admin:
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
     # media - static files to be served from development server
-    # (r'^xspf_player/(?P<path>.*)$', 'django.views.static.serve',
+    # url(r'^xspf_player/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': '/home/albert/xspf_player'}),
-    # (r'^files/(?P<path>.*)$', 'django.views.static.serve',
+    # url(r'^files/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': '/home/albert/django/pythoneer/files'}),
-    # ( r'^mp3/(?P<path>.*)$', 'django.views.static.serve',
+    # url( r'^mp3/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': '/home/albert/music/mp3'}),
-    # (r'^med/(?P<path>.*)$', 'django.views.static.serve',
+    # url(r'^med/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': '/home/albert/music/med'}),
-    # (r'^mid/(?P<path>.*)$', 'django.views.static.serve',
+    # url(r'^mid/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': '/home/albert/music/mid'}),
-    # (r'^mod/(?P<path>.*)$', 'django.views.static.serve',
+    # url(r'^mod/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': '/home/albert/music/mod'}),
-    # (r'^xm/(?P<path>.*)$', 'django.views.static.serve',
+    # url(r'^xm/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': '/home/albert/music/xm'}),
-    # (r'^mm/(?P<path>.*)$', 'django.views.static.serve',
+    # url(r'^mm/(?P<path>.*)$', 'django.views.static.serve',
     #    {'document_root': '/home/albert/music/mm'}),
 )
